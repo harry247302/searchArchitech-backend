0f76a103ab94c2e1af9065be4e9a620b4a3feadf
shadab <shadab.shaikh247302@gmail.com>
2025-07-11 16:26:58 +0530

    cloudinary completge

--- controllers/Cloudinary.controller.js
+++ controllers/Cloudinary.controller.js
@@ -6,15 +6,13 @@ const upload = multer({ dest: 'uploads/' });
 
 const cloudinary_setup = async (req,res,next)=>{
     try {
-        console.log(req);
-        
         const result=await cloudinary.uploader.upload(req.file.path,{
             folder:'uploads'
         });
         fs.unlinkSync(req.file.path);
-        console.log(result);
+        console.log(result.secure_url);
         
-        res.json({url:result})
+        res.json({url:result.secure_url})
     } catch (error) {
         console.log(error)
         next(error)
